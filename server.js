aed6e5ca792b173005707e7122e02a880dabf1d3
juanpigonzalez72@gmail.com <juanpigonzalez72@gmail.com>
2024-10-18 13:45:10 -0300

    se sube el server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors());  // Habilita CORS
 
 // Conexión a la base de datos MySQL
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || '127.0.0.1',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'trivia'
